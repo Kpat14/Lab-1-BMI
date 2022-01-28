2325881b740dca52e3a2cc90f5e88933bc34a210
user <Kakshil.14@gmail.com>
2022-01-28 16:15:20 -0500

    ok

--- app/src/main/java/ca/yorku/bmi/Model/BMIModel.java
+++ app/src/main/java/ca/yorku/bmi/Model/BMIModel.java
@@ -1,11 +1,11 @@
 package ca.yorku.bmi.Model;
 
 public class BMIModel {
-    public static double toDouble(String s){
-        return Double.parseDouble(s);
+    public static double toDouble(String input){
+        return Double.parseDouble(input);
     }
-    public static double getBMI(double w, double h){
-        return w/(h*h);
+    public static double getBMI(double weight, double height){
+        return weight/(height*height);
     }
     public static String formatBMI(double bmi){
         return String.format("%.2f",bmi);
